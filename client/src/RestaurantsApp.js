86ff78af6c7399ec9c166f84471a115e30d0efa8
Max Manayan <maximillian521@gmail.com>
2021-03-08 16:19:40 -0700

    set restaurants controller path

--- client/src/RestaurantsApp.js
+++ client/src/RestaurantsApp.js
@@ -1,4 +1,20 @@
+import {useState, useEffect} from "react";
+import axios from "axios";
+
 const RestaurantsApp = () => {
+  const [restaurants, setRestaurants] = useState([])
+
+
+  const getRestaurants = async () => {
+    try {
+      let res = await axios.get("/restaurants")
+      console.log(res)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+
   return(
     <div>
       <h1>Restaurants App Page</h1>
