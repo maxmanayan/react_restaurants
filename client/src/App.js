e0f4183ca899828d6961b4a37afc678e133b0134
Callice Austin <callice.a@gmail.com>
2021-03-08 15:16:39 -0700

    rails route

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container } from "semantic-ui-react";
+import Navbar from "./Components/Navbar";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
