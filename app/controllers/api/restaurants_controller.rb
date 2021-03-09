078831d4b79179718a7dfdfd1679e1fdf8bbbdb2
Keyshaonline <keyshaleonidas01@gmail.com>
2021-03-08 19:48:39 -0700

    crudbackend_controller_setup

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -5,3 +5,31 @@ class Api::RestaurantsController < ApplicationController
   end
 
 end
+
+def show 
+  render json: restaurant.find(params[:id])
+
+end
+
+def create 
+   restaurant = Restaurant.new(restaurant_params)
+   restaurant.rating = 0
+   if restaurant.save
+      render json: restaurant
+   else
+     render json: { errors: restaurant.errors }, status: :unprocessable_entity 
+   end
+end
+
+def update
+ restaurant = Restaurant.find(params[:id])
+ if restaurant.update(restaurant_params)
+  render json: restaurant
+else
+  render json: {errors: restaurant.errors}, status: :unprocessable_entity
+end
+private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :review)
+  end
+end
